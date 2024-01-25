bef5e9559571233cd14484981babda0001a66595
rayhanalmim <rayhanalmim1@gmail.com>
2024-01-25 16:57:15 +0600

    update api endpoint added successfully

--- src/api/manageProperty/contrillers/updateProperty.js
+++ src/api/manageProperty/contrillers/updateProperty.js
@@ -1,5 +1,12 @@
 const PropertyCollection = require("../../../models/productSchema");
 
 const updateProperty=async(req, res)=>{
+    const propertyId = req.query.propertyId;
+    const { propertyName, image, quantity, price, bedRoom, bathRoom, livingRoom, propertyType, propertyDetails, location } = req.body;
+    const update = await PropertyCollection.updateOne(
+        { _id: new Object(propertyId) },
+        { $set: { propertyName: propertyName, image: image, quantity: quantity, price: price, bedRoom: bedRoom, bathRoom: bathRoom, livingRoom: livingRoom, propertyType: propertyType, propertyDetails: propertyDetails, location: location} },
+    );
+    res.send(update);
 };
 module.exports=updateProperty;
