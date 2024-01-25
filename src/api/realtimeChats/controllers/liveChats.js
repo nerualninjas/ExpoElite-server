627ae05a64834682b51ba63370029c51dc5bc7f2
rayhanalmim <rayhanalmim1@gmail.com>
2024-01-25 13:27:20 +0600

    basic structure of chat endpoint implemented

--- src/api/realtimeChats/controllers/liveChats.js
+++ src/api/realtimeChats/controllers/liveChats.js
@@ -1,8 +1,27 @@
 const ChatsCollection = require("../../../models/ChatLogs");
 
-const sendMessage=async(req, res)=>{
-   const message = req.body;
-   const insert = await ChatsCollection.create(message);
-   res.send(insert);
+const sendMessage = async (req, res) => {
+   const senderEmail = req.query.email;
+   const reciverEmail = req.query.email;
+   const { message } = req.body;
+   const firstQuery = {
+      'conversationBetween': senderEmail + "&" + reciverEmail,
+   }
+   const secondQuery = {
+      'conversationBetween': reciverEmail + "&" + senderEmail,
+   }
+   const isConversationExists = await ChatsCollection.findOne({
+      $or: [firstQuery, secondQuery],
+   });
+   if(isConversationExists){
+      const update = await ChatsCollection.updateOne(
+         { $or: [firstQuery, secondQuery] },
+         {
+           $push: { chatLogs: message },
+         },
+       );
+       return res.send({message: `messege added in previous conversation. logs: ${update}`});
+   }
+   
 };
-module.exports=sendMessage;
+module.exports = sendMessage;
