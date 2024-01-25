bc2e95e8474c0aa87722f75cf542793d5af89c35
rayhanalmim <rayhanalmim1@gmail.com>
2024-01-25 16:37:32 +0600

    property manage releted all required route implemented

--- src/api/manageProperty/contrillers/index.js
+++ src/api/manageProperty/contrillers/index.js
@@ -2,10 +2,12 @@ const addProperty = require('./addProperty');
 const getProperty = require('./getProperty');
 const getAllProperty = require('./getAllProperty');
 const updateProperty = require('./deleteProperty');
+const deleteProperty = require('./deleteProperty')
 
 module.exports={
     addProperty,
     getProperty,
     getAllProperty, 
-    updateProperty
+    updateProperty,
+    deleteProperty,
 }
