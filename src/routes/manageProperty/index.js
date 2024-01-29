c002499c6c91c22329d28221b41f42d4a2320cd9
ashiq73 <ashiq.buet73@gmail.com>
2024-01-29 17:14:09 +0600

    product get,post and delete and update api initigration done

--- src/routes/manageProperty/index.js
+++ src/routes/manageProperty/index.js
@@ -1,11 +1,17 @@
-const express = require('express');
-const { addProperty, getProperty, getAllProperty, updateProperty, deleteProperty } = require('../../api/manageProperty/contrillers');
+const express = require("express");
+const {
+  addProperty,
+  getProperty,
+  getAllProperty,
+  updateProperty,
+  deleteProperty,
+} = require("../../api/manageProperty/contrillers");
 const router = express.Router();
 
-router.post('/addProperty', addProperty);
-router.get('/getProperty', getProperty);
-router.get('/getAllProperty', getAllProperty);
-router.put('/updateProperty', updateProperty);
-router.delete('deleteProperty', deleteProperty);
+router.post("/addProperty", addProperty);
+router.get("/getProperty/:id", getProperty);
+router.get("/getAllProperty", getAllProperty);
+router.patch("/updateProperty/:id", updateProperty);
+router.delete("/deleteProperty/:id", deleteProperty);
 
-module.exports=router;
+module.exports = router;
