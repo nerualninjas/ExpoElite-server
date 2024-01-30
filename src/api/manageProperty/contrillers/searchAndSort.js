bb0566aeadaceae44f242b022a9be35389151158
rayhanalmim <rayhanalmim1@gmail.com>
2024-01-30 14:40:04 +0600

    implement search and sorting functionality and check the api with postman

---
+++ src/api/manageProperty/contrillers/searchAndSort.js
@@ -0,0 +1,29 @@
+const PropertyCollection = require("../../../models/productSchema");
+
+const searchAndSort=async(req, res)=>{
+    try {
+        let query = {};
+        if (req.query.location) {
+          query.location = { $regex: new RegExp(req.query.location, 'i') };
+        }
+        if (req.query.minPrice || req.query.maxPrice) {
+          query.price = {};
+          if (req.query.minPrice) {
+            query.price.$gte = parseInt(req.query.minPrice);
+          }
+          if (req.query.maxPrice) {
+            query.price.$lte = parseInt(req.query.maxPrice);
+          }
+        }
+        if (req.query.propertyType) {
+          query.propertyType = req.query.propertyType;
+        }
+        const properties = await PropertyCollection.find(query).sort({ location: 1, price: 1 });
+        res.json(properties);
+        
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+};
+module.exports=searchAndSort;
