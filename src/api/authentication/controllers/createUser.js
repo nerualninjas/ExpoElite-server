189235c1e9f787d765cf663e38c66e9f49d912a3
ashiq73 <ashiq.buet73@gmail.com>
2024-01-30 23:42:43 +0600

    duplicate user handle

--- src/api/authentication/controllers/createUser.js
+++ src/api/authentication/controllers/createUser.js
@@ -2,12 +2,12 @@ const UserCollection = require("../../../models/user");
 
 const createUser = async (req, res) => {
   const user = req.body;
-  const query = { email: user.userEmail };
+  const query = { userEmail: user.userEmail };
   const existingUser = await UserCollection.findOne(query);
   if (existingUser) {
     return res.send({ message: "user already register", insertedId: null });
   }
   const create = await UserCollection.create(user);
-  res.send(create);
+  res.send({ message: "user registration successfully", insertedId: 1 });
 };
 module.exports = createUser;
