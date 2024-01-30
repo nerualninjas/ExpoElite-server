2e25b48dfb51ced241c68ec98d7ff1952f9cf1c2
rayhanalmim <rayhanalmim1@gmail.com>
2024-01-30 17:46:08 +0600

    hide some code for error

--- src/routes/media/index.js
+++ src/routes/media/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { uploadImage } = require("../../api/uploadMedia/controller");
-const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+// const multer = require('multer');
+// const upload = multer({ dest: 'uploads/' });
+// adlsjfklsd
 
 const router = express.Router();
 
-router.post("/uploadImage", upload.single('imagePath'), uploadImage);
+// router.post("/uploadImage", upload.single('imagePath'), uploadImage);
 
 module.exports = router;
