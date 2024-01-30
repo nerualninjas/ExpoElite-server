8c86404d2954d6b1998edc4ba901424f0136e785
ashiq73 <ashiq.buet73@gmail.com>
2024-01-30 23:26:54 +0600

    user duplicate condition

--- src/api/authentication/controllers/createUser.js
+++ src/api/authentication/controllers/createUser.js
@@ -2,6 +2,11 @@ const UserCollection = require("../../../models/user");
 
 const createUser = async (req, res) => {
   const user = req.body;
+  const query = { email: user.userEmail };
+  const existingUser = await UserCollection.findOne(query);
+  if (existingUser) {
+    return res.send({ message: "user already register", insertedId: null });
+  }
   const create = await UserCollection.create(user);
   res.send(create);
 };
