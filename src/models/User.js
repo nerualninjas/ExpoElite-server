c06d69a1203a3c683d8ba3b1988850afe8791efb
ashiqee <ashiq.buet73@gmail.com>
2024-02-06 21:00:46 +0600

    git user updated

--- src/models/User.js
+++ src/models/User.js
@@ -1,31 +1,54 @@
-const { Schema, model } = require("mongoose");
-
-// ------------------------------------UsingSchema----------------------------------------
-
-const userSchema = new Schema({
-  userName: {
-    type: String,
-    required: true,
-  },
-  userEmail: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  userPhoto: {
-    type: String,
-    required: true,
-  },
-  userRole: {
-    type: String,
-    enum: ["user", "Admin", "Seller", "Buyer"],
-    required: true,
-  },
-});
+const mongoose = require ("mongoose")
 
 
-const userCollection = model("users", userSchema);
+const userSchema = new mongoose.Schema({
+
+    userEmail:{
+        type:String,
+        require:true,
+    },
+    userName:{
+        type:String,
+        require:true,
+    },
+    userPhoto:{
+        type:String,
+        require:true,
+    },
+ 
+    userMemberShip:{
+        type:String,
+        enum: ['free',"Premium","ultra"],
+
+    },
+    memberShipStatus:{
+        type:String,
+        enum: ['free',"Active","Renew","deactivated"],
+
+    },
+    userRole:{
+        type:String,
+        enum: ['user',"Admin","Seller"],
+        require:true,
+    },
+    roleStatus: {
+        type: String,
+        enum: [
+          "no request",
+          "pending request for Seller",
+          "Approved",
+        ],
+      },
+      userStatus: {
+        type: String,
+        enum: ["Active", "Blocked"],
+      },
+
+  
+
+
+})
+
+const userCollection = mongoose.model("users", userSchema);
 
 module.exports = userCollection;
