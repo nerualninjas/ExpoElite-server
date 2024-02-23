e5a791b55923d91a5730c22cbc39204457b42e45
sabrinara <saashid18@gmail.com>
2024-02-23 14:31:50 +0600

    get all reviews route setup done

--- src/api/reviewProperty/controllers/index.js
+++ src/api/reviewProperty/controllers/index.js
@@ -1,8 +1,10 @@
 const addReview = require("./addReview");
 const getReview = require("./getReview");
+const getAllReviews = require("./getAllReviews");
 
 
 module.exports = {
     addReview,
-    getReview
+    getReview,
+    getAllReviews
 }
--- src/routes/reviewProperty/index.js
+++ src/routes/reviewProperty/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { addReview, getReview } = require("../../api/reviewProperty/controllers");
+const { addReview, getReview, getAllReviews} = require("../../api/reviewProperty/controllers");
+
 const router = express.Router();
 
 
 router.post("/addReview", addReview);
 router.get("/getReview/:email", getReview);
+router.get("/getAllReviews", getAllReviews);
 
 module.exports = router;
