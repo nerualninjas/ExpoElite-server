7a8619351f471185d4fb3b61ee9e12608387450b
aubonee <chytahsin.2210@gmail.com>
2024-02-25 14:01:46 +0600

    server crash issue solved

--- src/api/manageProperty/contrillers/getAllProperty.js
+++ src/api/manageProperty/contrillers/getAllProperty.js
@@ -2,8 +2,22 @@ const PropertyCollection = require("../../../models/productSchema");
 
 const getAllProperty = async (req, res) => {
   const { page, limit } = req.query;
-  console.log(page, limit);
-  const property = await PropertyCollection.find().skip((page - 1) * limit).limit(limit)
-  res.send(property);
+  
+  // Parse the limit value to an integer
+  const parsedLimit = parseInt(limit, 10);
+
+  console.log(page, parsedLimit);
+
+  try {
+    const property = await PropertyCollection.find()
+      .skip((page - 1) * parsedLimit)
+      .limit(parsedLimit);
+
+    res.send(property);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 };
+
 module.exports = getAllProperty;
