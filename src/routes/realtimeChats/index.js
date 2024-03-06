eff2c9056823280d52fe4c3a5f7b4f77a82e7035
rayhanalmim <rayhanalmim1@gmail.com>
2024-03-06 16:51:01 +0600

    modify chat functionality

--- src/routes/realtimeChats/index.js
+++ src/routes/realtimeChats/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { sendMessage, getMessage } = require('../../api/realtimeChats/controllers');
+const { sendMessage, getMessage, getAllMessage } = require('../../api/realtimeChats/controllers');
 const router = express.Router();
 
 router.post('/sendMessage', sendMessage);
 router.get('/getMessage', getMessage);
+router.get('/getAllconver', getAllMessage);
 
 module.exports=router;
